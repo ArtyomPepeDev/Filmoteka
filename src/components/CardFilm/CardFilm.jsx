d41304ef2c692788b641d00d3dc80e43ae5963aa
pr1z0ng <131606803+pr1z0ng@users.noreply.github.com>
2024-01-22 20:17:45 +0200

    add movie card flex

--- src/components/CardFilm/CardFilm.jsx
+++ src/components/CardFilm/CardFilm.jsx
@@ -5,17 +5,12 @@ import { CardFilmWrapper, FilmTitle, FilmDescription } from "./CardFilm.styled";
 const CardFilm = ({ item }) => {
   return (
     <CardFilmWrapper>
-      <div>
-        <h2>Most Popular Movies</h2>
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
-          alt={item.title}
-        />{" "}
-        <FilmTitle>{item.title}</FilmTitle>
-        <p>Release Year: {new Date(item.release_date).getFullYear()}</p>
-        <p>Genres: {item.genres.map((genre) => genre.name).join(", ")}</p>
-        <FilmDescription>{item.overview}</FilmDescription>
-      </div>
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+        alt={item.title}
+      />{" "}
+      <FilmTitle>{item.title}</FilmTitle>
+      <p>Release Year: {new Date(item.release_date).getFullYear()}</p>
     </CardFilmWrapper>
   );
 };
--- src/components/CardList/CardList.styled.jsx
+++ src/components/CardList/CardList.styled.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
 export const CardListWrapper = styled.div`
-
+display: flex;
+flex-wrap: wrap;
+justify-content: space-around;
 `;
