3e85f4b3d39ccd8a6cca67a65e32a68109575fe4
Malberee <82259650+Malberee@users.noreply.github.com>
2024-01-22 21:20:13 +0200

    fix

--- src/components/CardFilm/CardFilm.jsx
+++ src/components/CardFilm/CardFilm.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { CardFilmWrapper, FilmTitle, FilmDescription, ImageStyle } from "./CardFilm.styled";
 
 const CardFilm = ({ item, genreList }) => {
-  const genre = genreList.find((genre) => genre.id === item.genre_ids[0])
+  const genres = genreList.filter((genre) => item.genre_ids.includes(genre.id))
 
   return (
     <CardFilmWrapper>
---
+++ src/components/Paginate/Paginate.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { PaginateWrapper } from './Paginate.styled'
+
+const Paginate = () => (
+    <PaginateWrapper>
+        Paginate Component
+    </PaginateWrapper>
+)
+
+Paginate.propTypes = {}
+
+Paginate.defaultProps = {}
+
+export default Paginate
